d1fa97176af5a08702498b84bc2777a5afede99e
Yudann <yudannnnn80@gmail.com>
2025-06-05 15:35:57 +0700

    feat: add product service

---
+++ src/services/products.service.ts
@@ -0,0 +1,9 @@
+import { findProductsApi } from "@/repositories/products.repository";
+import { ProductsApiResponse } from "@/types/product.type";
+
+export async function findProducts(): Promise<ProductsApiResponse[]> {
+  const products = await findProductsApi();
+  return products.map((product: ProductsApiResponse) => ({
+    ...product,
+  }));
+}
