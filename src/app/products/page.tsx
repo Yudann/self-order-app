e0c706f48e6a17585243169eb6e76e588a917320
Yudann <yudannnnn80@gmail.com>
2025-06-05 15:36:59 +0700

    feat: using api

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,13 @@
-export default function ProductsPage() {
-  return <div></div>;
+import ProductsList from "@/components/templates/ProductsList";
+import { findProducts } from "@/services/products.service";
+
+export const dynamic = "force-dynamic";
+
+export default async function ProductsPage() {
+  const data = await findProducts();
+  return (
+    <div className="mt-20">
+      <ProductsList data={data} />
+    </div>
+  );
 }
