b23813b1423006c85da5ee06eb9b89ae1f72e496
Yudann <yudannnnn80@gmail.com>
2025-06-05 15:36:30 +0700

    feat: add product list

---
+++ src/components/templates/ProductsList.tsx
@@ -0,0 +1,23 @@
+// components/templates/ProductsList.tsx
+import ProductCard from "../fragments/ProductCard";
+import { ProductsApiResponse } from "@/types/product.type";
+
+interface ProductsListProps {
+  data: ProductsApiResponse[];
+}
+
+export default function ProductsList({ data }: ProductsListProps) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+      {data.map((product) => (
+        <ProductCard
+          key={product.id}
+          id={product.id}
+          productImage={product.productImage}
+          productName={product.productName}
+          price={product.price}
+        />
+      ))}
+    </div>
+  );
+}
