b1aa5edd31db97c7f24041271f8a15dc522c34ab
Yudann <yudannnnn80@gmail.com>
2025-06-05 15:54:51 +0700

    feat: add error handling

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,10 +4,33 @@ import { findProducts } from "@/services/products.service";
 export const dynamic = "force-dynamic";
 
 export default async function ProductsPage() {
-  const data = await findProducts();
-  return (
-    <div className="mt-20">
-      <ProductsList data={data} />
-    </div>
-  );
+  try {
+    const data = await findProducts();
+
+    if (!data.length) {
+      return (
+        <h1 className="mt-20 text-center text-xl">Produk belum tersedia</h1>
+      );
+    }
+
+    return (
+      <div className="mt-20">
+        <ProductsList data={data} />
+      </div>
+    );
+  } catch (error: unknown) {
+    // Explicitly type the error as unknown
+    let errorMessage = "Produk tidak ditemukan karena server error, bung.";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    return (
+      <h1 className="mt-20 text-center text-xl text-red-600 font-bold">
+        Gagal Fetch data dengan error: <br />
+        {errorMessage}
+      </h1>
+    );
+  }
 }
