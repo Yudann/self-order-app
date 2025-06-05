60013ebeb913a95d8a95cdae1fbd451ae90a5ce5
Yudann <yudannnnn80@gmail.com>
2025-06-05 14:47:23 +0700

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  env:  'development',
+  appName: 'Self Order App',
+  appShortName: 'Self Order App',
+  publicAssetUrl: 'http://localhost:8081/uploads/',
+  assetsUrl: 'http://localhost:8081/uploads/',
+  foodyAppService: 'http://localhost:8081/api',
+};
+
+export default config;
