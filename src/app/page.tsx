62e52d8f00466f0c1bd6ad082aa2da1c96844767
Yudann <yudannnnn80@gmail.com>
2025-06-05 13:34:51 +0700

    style: custom color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -5,17 +6,11 @@ export default function Home() {
     <div className="min-w-screen min-h-screen w-full h-full flex flex-col justify-center items-center">
       <h1 className="text-xl font-bold">Welcome To Self Order App entahlah</h1>
       <div className="flex flex-col items-center justify-center gap-4 mt-5">
-        <Link
-          href="/food"
-          className="px-8 py-2 border border-gray-200 rounded-xl bg-orange-300 hover:bg-orange-400 transition cursor-pointer text-lg "
-        >
-          Food
+        <Link href="/food">
+          <Button variant="purple">Food</Button>
         </Link>
-        <Link
-          href="/drink"
-          className="px-8 py-2 border border-gray-200 rounded-xl bg-orange-300 hover:bg-orange-400 transition cursor-pointer text-lg "
-        >
-          Drink
+        <Link href="/drink">
+          <Button variant="purple">Drink</Button>
         </Link>
       </div>
     </div>
